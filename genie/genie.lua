a10ebe7d4509ed801a13cafec57917f1b083e6a9
Milos Tosic <milostosic77@gmail.com>
2018-07-23 15:02:30 +0800

    Update build scripts to reflect changes in build project

--- genie/genie.lua
+++ genie/genie.lua
@@ -9,44 +9,11 @@ while string.len(currPath) > 2 do
 	if os.isfile(currPath .. "/build/build.lua") then dofile (currPath .. "/build/build.lua") break end
 end
 
-function rmemExtraConfig_linker()
-	configuration { "gmake" }
-		if ("mingw-gcc"   == _OPTIONS["gcc"]) or -- on windows, we patch heap functions, no need to wrap malloc family of funcs
-		   ("mingw-clang" == _OPTIONS["gcc"]) then -- on windows, we patch heap functions, no need to wrap malloc family of funcs
-			linkoptions { "-Wl,--wrap=_malloc_init--export-all-symbols" }
-			links { "psapi" }
-		else 
-			linkoptions { "-Wl,--wrap=_malloc_init,--wrap=malloc,--wrap=realloc,--wrap=calloc,--wrap=free,--wrap=memalign,--wrap=reallocalign" }
-			if not os.is("linux") and not (_OPTIONS["gcc"] == "orbis") then
-				linkoptions { "--export-all-symbols,--wrap=_expand" }
-			end
-		end
-		
-	configuration { "vs*", "orbis" }
-		linkoptions { "-Wl,--wrap=_malloc_init,--wrap=malloc,--wrap=realloc,--wrap=calloc,--wrap=free,--wrap=memalign,--wrap=reallocalign" }
-
-	configuration { "vs*", "not orbis" }
-		linkoptions { "/ENTRY:rmemEntry" }
-
-	configuration {}
-end
-
-function rmemExtraConfig_manual()
-	configuration { "windows", "gmake" }
-		if not (_OPTIONS["gcc"] == "orbis") then
-			linkoptions { "-Wl,--export-all-symbols" }
-		end
-		if "mingw" == _OPTIONS["gcc"] then
-			links { "psapi" }
-		end
-	configuration {}		
-end
-
 solution "rmem"
 	configurations { "debug", "release", "retail" }
 	setPlatforms()
 
 	addProject_lib("rmem")
-	addProject_lib_sample("rmem", "linker", false, rmemExtraConfig_linker)
-	addProject_lib_sample("rmem", "manual", false, rmemExtraConfig_manual)
+	addProject_lib_sample("rmem", "linker", false)
+	addProject_lib_sample("rmem", "manual", false)
 
--- genie/rmem.lua
+++ genie/rmem.lua
@@ -3,6 +3,39 @@
 -- License: http://www.opensource.org/licenses/BSD-2-Clause
 --
 
+function projectExtraConfigExecutable_linker()
+	configuration { "gmake" }
+		if ("mingw-gcc"   == _OPTIONS["gcc"]) or -- on windows, we patch heap functions, no need to wrap malloc family of funcs
+		   ("mingw-clang" == _OPTIONS["gcc"]) then -- on windows, we patch heap functions, no need to wrap malloc family of funcs
+			linkoptions { "-Wl,--wrap=_malloc_init--export-all-symbols" }
+			links { "psapi" }
+		else 
+			linkoptions { "-Wl,--wrap=_malloc_init,--wrap=malloc,--wrap=realloc,--wrap=calloc,--wrap=free,--wrap=memalign,--wrap=reallocalign" }
+			if not os.is("linux") and not (_OPTIONS["gcc"] == "orbis") then
+				linkoptions { "--export-all-symbols,--wrap=_expand" }
+			end
+		end
+		
+	configuration { "vs*", "orbis" }
+		linkoptions { "-Wl,--wrap=_malloc_init,--wrap=malloc,--wrap=realloc,--wrap=calloc,--wrap=free,--wrap=memalign,--wrap=reallocalign" }
+
+	configuration { "vs*", "not orbis" }
+		linkoptions { "/ENTRY:rmemEntry" }
+
+	configuration {}
+end
+
+function projectExtraConfigExecutable_manual()
+	configuration { "windows", "gmake" }
+		if not (_OPTIONS["gcc"] == "orbis") then
+			linkoptions { "-Wl,--export-all-symbols" }
+		end
+		if "mingw" == _OPTIONS["gcc"] then
+			links { "psapi" }
+		end
+	configuration {}		
+end
+
 function projectAdd_rmem() 
 	addProject_lib("rmem")
 end
