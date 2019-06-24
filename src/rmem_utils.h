bea578be087697ee43be902be4858df3fbc415bf
Milos Tosic <milostosic77@gmail.com>
2019-06-24 09:15:30 +0800

    Remove dead code

--- src/rmem_utils.h
+++ src/rmem_utils.h
@@ -28,64 +28,6 @@ inline static uint32_t rotl32( uint32_t x, int8_t r )
 
 namespace rmem {
 
-//-----------------------------------------------------------------------------
-// MurmurHash3 was written by Austin Appleby, and is placed in the public
-// domain. The author hereby disclaims copyright to this source code.
-
-
-	//--------------------------------------------------------------------------
-	/// Murmur3 hashing
-	//--------------------------------------------------------------------------
-	static inline uint32_t hashMurmur3(const void* _key, uint32_t _len, uint32_t _seed = 0)
-	{
-		const uint8_t * data = (const uint8_t*)_key;
-		const int nblocks = _len / 4;
-
-		uint32_t h1 = _seed;
-
-		uint32_t c1 = 0xcc9e2d51;
-		uint32_t c2 = 0x1b873593;
-
-		const uint32_t * blocks = (const uint32_t *)(data + nblocks*4);
-
-		for (int i=-nblocks; i; ++i)
-		{
-			uint32_t k1 = blocks[i];
-
-			k1 *= c1;
-			k1 = ROTL32(k1,15);
-			k1 *= c2;
-    
-			h1 ^= k1;
-			h1 = ROTL32(h1,13); 
-			h1 = h1*5+0xe6546b64;
-		}
-
-		const uint8_t * tail = (const uint8_t*)(data + nblocks*4);
-
-		uint32_t k1 = 0;
-
-		switch(_len & 3)
-		{
-		case 3: k1 ^= tail[2] << 16;
-		case 2: k1 ^= tail[1] << 8;
-		case 1: k1 ^= tail[0];
-				k1 *= c1; k1 = ROTL32(k1,15); k1 *= c2; h1 ^= k1;
-		};
-
-		h1 ^= _len;
-		h1 ^= h1 >> 16;
-		h1 *= 0x85ebca6b;
-		h1 ^= h1 >> 13;
-		h1 *= 0xc2b2ae35;
-		h1 ^= h1 >> 16;
-
-		return h1;
-	} 
-
-	//--------------------------------------------------------------------------
-	/// Calculate a string hash, suitable for short strings
-	//--------------------------------------------------------------------------
 	static inline uint32_t hashStr(const char* _string)
 	{
 	   uint32_t	h;
