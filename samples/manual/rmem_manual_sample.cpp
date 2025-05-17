0516a9c8738dca21f9da129205e909a489900f78
milos.tosic <milos.tosic@1047games.com>
2025-05-17 21:52:21 +0200

    Check for clang using built-in macro

--- samples/manual/rmem_manual_sample.cpp
+++ samples/manual/rmem_manual_sample.cpp
@@ -51,7 +51,7 @@ size_t getReallocOverhead(void* _ptr)
 	return 8; // fake fixed overhead
 }
 
-#if RTM_COMPILER_CLANG
+#if defined(__clang__)
 // disable warning: pointer ‘prevPtr’ used after ‘void* realloc
 // This is intentional behaviour here
 #pragma clang diagnostic push
@@ -76,7 +76,7 @@ struct Allocator
 	}
 };
 
-#if RTM_COMPILER_CLANG
+#if defined(__clang__)
 #pragma clang diagnostic pop
 #endif
 
