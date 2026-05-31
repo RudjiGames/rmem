1f412df1b687fd96fa652294084b371df1daeb9a
Milos Tosic <milos@rudji.com>
2026-05-31 02:38:12 +0200

    Update copyright

--- src/rmem_hook.h
+++ src/rmem_hook.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------//
-/// Copyright 2025 Milos Tosic. All Rights Reserved.                       ///
+/// Copyright 2026 Milos Tosic. All Rights Reserved.                       ///
 /// License: http://www.opensource.org/licenses/BSD-2-Clause               ///
 //--------------------------------------------------------------------------//
 
